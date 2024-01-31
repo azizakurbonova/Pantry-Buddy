13f352eaeb7725502b9d2ba1e211cb243bb6bac6
azizakurbonova <138236373+azizakurbonova@users.noreply.github.com>
2024-01-30 20:12:31 -0500

    updating entire repo with clean flutter project

---
+++ app/src/main/java/com/a11/pantrybuddy/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.a11.pantrybuddy
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.a11.pantrybuddy.ui.theme.PantryBuddyTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PantryBuddyTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+            text = "Hello $name!",
+            modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    PantryBuddyTheme {
+        Greeting("Android")
+    }
+}
