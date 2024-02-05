81f3538665ef289d3906be486a08c0f652562839
Aidyn Wang <amlwang231@gmail.com>
2024-02-05 04:31:30 -0500

    Add main import

--- pantrybuddy/lib/main.dart
+++ pantrybuddy/lib/main.dart
@@ -1,5 +1,7 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:pantrybuddy/auth/main_page.dart';
 import 'package:pantrybuddy/firebase_options.dart';
 
 void main() async {
